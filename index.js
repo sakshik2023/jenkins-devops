a46b5c827e8512f9f9032d0aaabcbfa9d9d4d483
Sakshi71125 <sakshikumari71425@gmail.com>
2026-05-09 13:10:33 +0530

    update the port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
  
 const app = express();
- const PORT = process.env.PORT || 3000;
+ const PORT =  3000;
 // Add middleware to parse JSON bodies
 app.use(express.json());
  
